1f6163e2c7cdd4fbaca1c2f4d59d0ace57e26836
Gede Hari <harikresnagede@gmail.com>
2021-01-11 16:30:24 +0800

    Screenshot thing so i can debug easier

--- src/global.gd
+++ src/global.gd
@@ -4,3 +4,12 @@ var default_speed: int = 325
 var current_speed: int = default_speed
 
 var player_x: int
+
+var main: CanvasLayer
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("screenshot"):
+		var image: Image = get_viewport().get_texture().get_data()
+		image.flip_y()
+		image.save_png("user://s.png")
